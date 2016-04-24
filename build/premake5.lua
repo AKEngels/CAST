85e322b7c6976e9b4181f3597f0d4c180d94c1df
Dustin Kaiser <Dustin.Kaiser@gmx.net>
2016-04-24 09:52:45 +0300

    Add premake5 to CAST
    Finally a proper build environment :D see wiki entry on"How to build
    CAST" for more information on how to use it correctly. Due to the new
    VS-Solution I had to change char->wchar_t in the Mopac-System-Call. Not
    quite sure why this surfaced now, but since CreateProcess() demanded
    wchar_t I just changed it.

---
+++ build/premake5.lua
@@ -0,0 +1,103 @@
+-- premake5.lua
+-- Get Premake5 via https://premake.github.io/
+--
+-- CAST automatic build configuration
+-- Targeted at VS2015 and our own Linux-Cluster "ECPC"
+--
+-- Build for windows: "premake5 vs2015"
+-- Open CAST.sln in project/
+--
+-- Build for Linux on ECPC without MPI: "premake5 gmake" 
+-- Run "make config=release_x64 CXX=g++-5" in project/
+--
+-- Build for Linux on ECPC wit MPI: "premake5 gmake --mpi" 
+-- Run "make config=release_x64 CXX=mpic++" in project/
+--
+
+newoption {
+   trigger     = "mpi",
+   description = "Add MPI Flag to gmake makefiles"
+}
+
+workspace "CAST"
+	configurations { "Debug", "Release", "Testing" }
+    location "../project"
+   	platforms { "x86", "x64"}
+	filter { "platforms:x86" }
+		architecture "x32"
+	filter { "platforms:x64" }
+		architecture "x64"
+	filter{}
+
+project "CAST"
+	kind "ConsoleApp"
+	language "C++"
+	targetdir "../build/"
+	files { "../src/**.h", "../src/**.cc" }
+	
+	vpaths { ["Headers"] = "../src/**.h" , ["Sources"] = "../src/**.cc"}
+
+
+	configuration "gmake" 
+		linkoptions { "-fopenmp" }
+		filter { "options:mpi" }
+			buildoptions { "-Wextra", "-Wall", "-std=c++0x", "-pedantic", "-fopenmp", "-static", "-DTERACHEM_MPI" }
+		filter {}
+		buildoptions { "-Wextra", "-Wall", "-std=c++0x", "-pedantic", "-fopenmp", "-static", }
+		filter { "configurations:Release" }
+			removefiles { "../src/tests/**.cc", "./src/test/**.h"}
+			optimize "Full"
+		filter { "configurations:Release",  "platforms:x86", "action:gmake"}
+		targetname "CAST_linux_x86_release"	
+		filter { "configurations:Release",  "platforms:x64", "action:gmake"}
+		targetname "CAST_linux_x64_release"	
+		
+		filter { "configurations:Testing" }
+			optimize "Debug"
+			defines { "GOOGLE_MOCK" }
+		filter { "configurations:Testing",  "platforms:x86", "action:gmake"}
+		targetname "CAST_linux_x86_testing"	
+		filter { "configurations:Testing",  "platforms:x64", "action:gmake"}
+		targetname "CAST_linux_x64_testing"			
+		
+		filter { "configurations:Debug" }
+			removefiles { "./src/tests/**.cc", "./src/test/**.h"}
+			defines { "CAST_DEBUG_DROP_EXCEPTIONS" }
+			optimize "Debug"
+			flags { "Symbols" }
+		filter { "configurations:Debug",  "platforms:x86", "action:gmake"}
+		targetname "CAST_linux_x86_debug"
+		filter { "configurations:Debug",  "platforms:x64", "action:gmake"}
+		targetname "CAST_linux_x64_debug"	
+		
+	configuration "vs2015"
+		targetname "CAST.exe" 
+		buildoptions { "/openmp" }
+		flags { "MultiProcessorCompile" }
+
+		filter { "configurations:Release" }
+			removefiles { "./src/tests/**.cc", "./src/test/**.h"}
+			optimize "Full"
+			flags { "LinkTimeOptimization" }
+		filter { "configurations:Release",  "platforms:x86", "action:vs2015"}
+		targetname "CAST_win_x86_release"
+		filter { "configurations:Release",  "platforms:x64", "action:vs2015"}
+		targetname "CAST_win_x64_release"		
+		
+		filter { "configurations:Testing" }
+			optimize "Debug"
+			defines { "GOOGLE_MOCK" }
+		filter { "configurations:Testing",  "platforms:x86", "action:vs2015"}
+		targetname "CAST_win_x86_testing"
+		filter { "configurations:Testing",  "platforms:x64", "action:vs2015"}
+		targetname "CAST_win_x64_testing"	
+			
+		filter { "configurations:Debug" }
+			removefiles { "./src/tests/**.cc", "./src/test/**.h"}
+			defines { "CAST_DEBUG_DROP_EXCEPTIONS" }
+			optimize "Debug"
+			flags { "Symbols" }
+		filter { "configurations:Debug",  "platforms:x86", "action:vs2015"}
+		targetname "CAST_win_x86_debug"
+		filter { "configurations:Debug",  "platforms:x64", "action:vs2015"}
+		targetname "CAST_win_x64_debug"	
