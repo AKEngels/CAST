85196578f19e7988da64dace6b6e12cf5f404cbe
neelix2606 <daniel.bellinger@uni-wuerzburg.de>
2016-05-10 08:45:47 +0200

    only minor changes to neb

--- src/neb.cc
+++ src/neb.cc
@@ -42,7 +42,7 @@ void neb::preprocess(ptrdiff_t &count)
   energies.resize(num_images);
   initial();
   final();
-  create(count);
+  create();
   if (Config::get().neb.CONSTRAINT_GLOBAL)run(count, image_remember, atoms_remember);
   else run(count);
 }
@@ -77,7 +77,7 @@ void neb::preprocess(ptrdiff_t &image, ptrdiff_t &count, const coords::Represent
   ts_pathstruc = ts_path;
   initial(start);
   final(fi);
-  create(count);
+  create();
   if (Config::get().neb.CONSTRAINT_GLOBAL)run(count, image_remember, atoms_remember);
   else run(count);
 
@@ -109,7 +109,7 @@ void neb::preprocess(ptrdiff_t &file, ptrdiff_t &image, ptrdiff_t &count, const 
   springconstant = Config::get().neb.SPRINGCONSTANT;
   initial(start);
   final(fi);
-  create(count);
+  create();
   if (Config::get().neb.CONSTRAINT_GLOBAL)run(count, image_remember, atoms_remember);
   else run(count);
 }
@@ -154,7 +154,7 @@ void neb::final(const coords::Representation_3D &fi)
   }
 }
 
-void neb::create(ptrdiff_t &count)
+void neb::create()
 {
 
   tempimage_ini = imagi[0];
@@ -590,7 +590,7 @@ void neb::calc_shift(void)
 {
   double diff{ 0.0 }, gridp{ 0.0 };
   std::vector<double> posx(num_images), posy(num_images), posz(num_images), gridx(num_images), gridy(num_images), gridz(num_images), shiftx(cPtr->size()), shifty(cPtr->size()), shiftz(cPtr->size());
-  double x{0.0}, y{0.0}, z{0.0}, pathlenx{0.0}, pathleny{0.0}, pathlenz{0.0};
+  double x{0.0}, y{0.0}, z{0.0};
   double distx{0.0}, disty{0.0}, distz{0.0};
   ptrdiff_t laf{0};
   std::vector <std::vector < std::vector < scon::c3<double> > > > position{ N };
--- src/neb.h
+++ src/neb.h
@@ -48,7 +48,7 @@ public:
   void final(void);
   void initial(const coords::Representation_3D &start);
   void final(const coords::Representation_3D &fi);
-  void create(ptrdiff_t &count);
+  void create();
   void get_energies(void);
   void calc_tau(void);
   void opt_io(ptrdiff_t &count);
@@ -328,11 +328,6 @@ public:
     }
 
   };
-
-
-
-
-
 
 
 
