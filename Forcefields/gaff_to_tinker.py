04e323b85456b9c3d416b4e10059d473471c0990
Susanne <Susanne@DESKTOP-NUJ8UQF>
2016-09-16 14:45:00 +0200

    updated torsion parameters in amber-gaff-forcefield

--- Forcefields/gaff_to_tinker.py
+++ Forcefields/gaff_to_tinker.py
@@ -17,6 +17,8 @@ until the next "print ausgabe". Copy the bonds from gaff.dat into a new file bin
 section of amber99.prm.
 
 Repeat the same for the angles (winkel.txt), torsions (torsions.txt) and improper torsions (impropers.txt).
+ATTENTION: For the torsions you have to copy the parameters where there is no "X" among the atoms in front of the other parameters!
+(assuming that CAST reads the parameters from up to down)
 """
 
 with open("atoms.txt") as atom_file:
@@ -74,11 +76,17 @@ for i, line in enumerate(winkel):
 with open("torsions.txt") as torsion_file:
     torsions = torsion_file.readlines()
 
+b_old = []
 for i, line in enumerate(torsions):
     b1 = line[:2]
     b2 = line[3:5]
     b3 = line[6:8]
     b4 = line[9:11]
+    b = [b1, b2, b3, b4]
+    if i < len(torsions)-1:
+        b_new = [torsions[i+1][:2], torsions[i+1][3:5], torsions[i+1][6:8], torsions[i+1][9:11]]
+    else:
+        b_new = []
     z2 = atomlist.index(b2) + CURRENT_NUMBER_OF_TINKERTYPES
     if b3 != "cb":
         z3 = atomlist.index(b3) + CURRENT_NUMBER_OF_TINKERTYPES
@@ -87,13 +95,28 @@ for i, line in enumerate(torsions):
             z4_list = range(CURRENT_NUMBER_OF_TINKERTYPES, endindex)
             for z1 in z1_list:
                 for z4 in z4_list:
-                    ausgabe = "torsion{:8d}{:5d}{:5d}{:5d}{:17.3f}{:7.1f}{:3d}".format(z1, z2, z3, z4, float(line[17:24]), float(line[31:38]), int(float(line[48:54])))
-                    print ausgabe
+                    if b != b_old and b != b_new:
+                        ausgabe = "torsion{:8d}{:5d}{:5d}{:5d}{:17.3f}{:7.1f}{:3d}".format(z1, z2, z3, z4, float(line[17:24]), float(line[31:38]), int(float(line[48:54])))
+                        print ausgabe
+                    elif b == b_new:
+                        line_old = line
+                    elif b == b_old:
+                        ausgabe1 = "torsion{:8d}{:5d}{:5d}{:5d}{:17.3f}{:7.1f}{:3d}".format(z1, z2, z3, z4, float(line_old[17:24]), float(line_old[31:38]), int(float(line_old[48:54])))
+                        ausgabe2 = "{:11.3f}{:7.1f}{:3d}".format(float(line[17:24]), float(line[31:38]), int(float(line[48:54])))
+                        print ausgabe1 + ausgabe2
         else:
             z1 = atomlist.index(b1) + CURRENT_NUMBER_OF_TINKERTYPES
             z4 = atomlist.index(b4) + CURRENT_NUMBER_OF_TINKERTYPES
-            ausgabe = "torsion{:8d}{:5d}{:5d}{:5d}{:17.3f}{:7.1f}{:3d}".format(z1, z2, z3, z4, float(line[17:24]), float(line[31:38]), int(float(line[48:54])))
-            print ausgabe
+            if b != b_old and b != b_new:
+                ausgabe = "torsion{:8d}{:5d}{:5d}{:5d}{:17.3f}{:7.1f}{:3d}".format(z1, z2, z3, z4, float(line[17:24]), float(line[31:38]), int(float(line[48:54])))
+                print ausgabe
+            elif b == b_new:
+                line_old = line
+            elif b == b_old:
+                ausgabe1 = "torsion{:8d}{:5d}{:5d}{:5d}{:17.3f}{:7.1f}{:3d}".format(z1, z2, z3, z4, float(line_old[17:24]), float(line_old[31:38]), int(float(line_old[48:54])))
+                ausgabe2 = "{:11.3f}{:7.1f}{:3d}".format(float(line[17:24]), float(line[31:38]), int(float(line[48:54])))
+                print ausgabe1 + ausgabe2
+    b_old = b
 
 with open("impropers.txt") as improper_file:
     impropers = improper_file.readlines()
