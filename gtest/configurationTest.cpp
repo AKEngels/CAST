32bb6a37b1adb080c043d746f5b55bb775a3ef89
mrnicegyu11 <Dustin.Kaiser@gmx.net>
2016-11-05 20:54:44 +0100

    fix configurationTests

--- gtest/configurationTest.cpp
+++ gtest/configurationTest.cpp
@@ -11,12 +11,13 @@ Purpose: Tests functions in configuration.cc
 #ifdef GOOGLE_MOCK
 #pragma once
 #include "../src/configuration.h"
+#include "../src/configurationHelperfunctions.h"
 #include "gtest/gtest.h"
 
 TEST(sorted_indices_from_cs_string, withProperInput)
 {
   std::string input = "1, 2, 3, 5";
-  std::vector<std::size_t> sorted = sorted_indices_from_cs_string(input);
+  std::vector<std::size_t> sorted = config::sorted_indices_from_cs_string(input);
   std::vector<std::size_t> compare = { 1u, 2u, 3u, 5u };
   ASSERT_EQ(sorted, compare);
 }
@@ -24,13 +25,13 @@ TEST(sorted_indices_from_cs_string, withProperInput)
 TEST(sorted_indices_from_cs_string, FailsWithFloatingPointInput)
 {
   std::string input = "1, 2.5, 3";
-  ASSERT_THROW(sorted_indices_from_cs_string(input), std::runtime_error);
+  ASSERT_THROW(config::sorted_indices_from_cs_string(input), std::runtime_error);
 }
 
 TEST(sorted_indices_from_cs_string, onlyCapturesIdenticalNumbersOnce)
 {
   std::string input = "1, 3, 5, 5, 1,";
-  std::vector<std::size_t> sorted = sorted_indices_from_cs_string(input);
+  std::vector<std::size_t> sorted = config::sorted_indices_from_cs_string(input);
   std::vector<std::size_t> compare = { 1u, 3u, 5u };
   ASSERT_EQ(sorted, compare);
 }
@@ -38,14 +39,14 @@ TEST(sorted_indices_from_cs_string, onlyCapturesIdenticalNumbersOnce)
 TEST(sorted_indices_from_cs_string, throwsWhenNegativeNumbersArePresent)
 {
   std::string input = "1, 3, -5,";
-  ASSERT_THROW(sorted_indices_from_cs_string(input), std::runtime_error);
+  ASSERT_THROW(config::sorted_indices_from_cs_string(input), std::runtime_error);
   std::string input2 = "1, -10.5";
-  ASSERT_THROW(sorted_indices_from_cs_string(input2), std::runtime_error);
+  ASSERT_THROW(config::sorted_indices_from_cs_string(input2), std::runtime_error);
 }
 TEST(sorted_indices_from_cs_string, takesRangeAndIsUnique)
 {
   std::string input = "0 - 1, 3-7, 4,";
-  std::vector<std::size_t> sorted = sorted_indices_from_cs_string(input);
+  std::vector<std::size_t> sorted = config::sorted_indices_from_cs_string(input);
   std::vector<std::size_t> compare = { 0u, 1u, 3u, 4u, 5u, 6u, 7u };
   ASSERT_EQ(sorted, compare);
 }
