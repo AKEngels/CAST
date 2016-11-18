8898b5869c8aab5f8500b19317953c9a63e20aa6
Susanne <susanneTsauer@gmx.net>
2016-11-18 10:42:25 +0100

    match default options in code and manual

--- src/configuration.h
+++ src/configuration.h
@@ -577,7 +577,7 @@ namespace config
     bool hooverHeatBath, veloScale, fep, track, optimize_snapshots, pressure, resume, umbrella, pre_optimize;
     molecular_dynamics(void) :
       timeStep(0.001), T_init(0.0), T_final(),
-      pcompress(), pdelay(), ptarget(),
+      pcompress(0.000046), pdelay(2.0), ptarget(1.0),
       num_steps(10000), num_snapShots(100), max_snap_buffer(50),
       refine_offset(0), restart_offset(0), usequil(), usoffset(),
       trackoffset(1), heat_steps(), spherical(), rattle(),
