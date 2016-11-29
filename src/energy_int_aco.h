26895fb11225bc558ba60e4740e9051b93c29932
Susanne <susanneTsauer@gmx.net>
2016-11-29 17:37:07 +0100

    add comments to energy_int_aco.h - does for some reason not show up in doxygen

--- src/energy_int_aco.h
+++ src/energy_int_aco.h
@@ -8,6 +8,7 @@ namespace energy
 {
   namespace interfaces
   {
+	  /**namespace for Amber, Charmm and Oplsaa*/
     namespace aco
     {
       
@@ -19,9 +20,9 @@ namespace energy
         inline bool factors (coords::float_type const rr, coords::float_type & r, coords::float_type & fQ, coords::float_type & fV);
       private:
         coords::float_type const c, s, cc, ss, cs;
-        nb_cutoff& operator=(nb_cutoff const&);
       };
 
+	  /**class for amber, charmm and oplsaa forcefield functions*/
       class aco_ff 
         : public interface_base
       {
@@ -32,21 +33,18 @@ namespace energy
         interface_base * move (coords::Coordinates * coord_object);
         // initialize using coordinates pointer
 
-        // update structure (account for topology or rep change)
+        /**update structure (account for topology or rep change, or just non-bonded list)*/
         void update (bool const skip_topology = false);
 
-        // Energy function
+        /**Energy function*/
         coords::float_type e (void);
-        // Energy+Gradient function
+        /**Energy+Gradient function*/
         coords::float_type g (void);
-        // Energy+Gradient+Hessian function
+        /** Energy+Gradient+Hessian function
+		at the moment does nothing because Hessians are not implemented yet*/
         coords::float_type h (void);
-        // Optimization in the intface or interfaced program
+        /** Optimization in the intface or interfaced program*/
         coords::float_type o (void);
-
-        // Virial Tensor
-
-        std::array<std::array<coords::float_type, 3>, 3> virial();
 
         // Output functions
         void print_E (std::ostream&) const;
@@ -66,11 +64,13 @@ namespace energy
         enum types { BOND = 0, ANGLE, IMPROPER, IMPTORSION, TORSION, MULTIPOLE, CHARGE, 
           POLARIZE, VDW, UREY, STRBEND, OPBEND, VDWC, SOLVATE, TYPENUM};
 
-        // Parameters
+        /** Parameters*/
         static ::tinker::parameter::parameters tp;
-        // Partial Gradients / Energies / Virials
+        /** Partial Gradients for every atom */ 
         std::array<coords::Representation_3D, TYPENUM> part_grad;
+		/** Partial Energies for every atom */
         std::array<coords::float_type, TYPENUM>  part_energy;
+		/** Partial Virials (are they implemented completely???)*/
         std::array<std::array<std::array<coords::float_type, 3>, 3>, TYPENUM> part_virial;
         
         ::tinker::parameter::parameters cparams;
@@ -81,9 +81,9 @@ namespace energy
         void	boxjump (void);
         inline std::ptrdiff_t sign (coords::float_type const) const;
  
-        // selection of the correct nonbonded function
+        /** selection of the correct nonbonded function*/
         template< ::tinker::parameter::radius_types::T RADIUS_TYPE > void   g_nb (void);
-        // nonbonded function including PME
+        /** nonbonded function including PME*/
         template< ::tinker::parameter::radius_types::T RADIUS_TYPE > void   g_nb_pme(void);
         template< ::tinker::parameter::radius_types::T RADIUS_TYPE > void   g_pme_QV_fep(void);
         template< ::tinker::parameter::radius_types::T RADIUS_TYPE > void   g_pme_QV_(void);
@@ -91,6 +91,9 @@ namespace energy
         void pre    (void);
         void post   (void);
 
+		/**calculate energy and gradients
+		DERIV = 0 -> energy
+		DERIV = 1 -> energy and gradients*/
         template<std::size_t DERIV> void calc (void);
 
         ::tinker::parameter::combi::vdwc const & vdwcpar (std::size_t const ta, std::size_t const tb, 
@@ -99,73 +102,90 @@ namespace energy
           return pm(cparams.contract_type(ta), cparams.contract_type(tb));
         }
 
-        // 12-interactions 
+        /** 12-interactions (bonds)*/
         template<std::size_t T_DERV> coords::float_type f_12 (void);
-        // 13-angles
+        /** 13-interactions (angles) */
         template<std::size_t T_DERV> coords::float_type f_13_a (void);
+		/** 13-interactions (urey-bradley) */
         template<std::size_t T_DERV> coords::float_type f_13_u (void);
 
+		/**14-interactions (torsions)*/
         template<std::size_t T_DERV> coords::float_type f_14 (void);
+		/**imptorsions interactions*/
         template<std::size_t T_DERV> coords::float_type f_it (void);
+		/**impropers interactions*/
         template<std::size_t T_DERV> coords::float_type f_imp (void);
 
-        // implicit solvation energy
+        /** implicit solvation energy */
         template<std::size_t T_DERV> coords::float_type solv();
 
-        // charge energy
+        /**charge energy */
         inline coords::float_type eQ (coords::float_type const C, coords::float_type const r) const;
-        // charge gradients
+        /** charge gradients */
         inline coords::float_type gQ (coords::float_type const C, coords::float_type const r, coords::float_type &dQ) const;
-        // charge gradients fep version
+        /** charge gradients fep version */
         inline coords::float_type gQ_fep(coords::float_type const C, coords::float_type const r, coords::float_type const c_out, coords::float_type &dQ) const;
-        // vdw energy
+        /** vdw energy */
         template< ::tinker::parameter::radius_types::T T_RADIUSTYPE > inline coords::float_type eV 
           (coords::float_type const E, coords::float_type const R, coords::float_type const r) const;
-        // vdw gradients
+        /** vdw gradients */
         template< ::tinker::parameter::radius_types::T T_RADIUSTYPE > inline coords::float_type gV 
           (coords::float_type const E, coords::float_type const R, coords::float_type const r, coords::float_type &dV) const;
+		/** vdw gradients FEP version*/
         template< ::tinker::parameter::radius_types::T T_RADIUSTYPE > inline coords::float_type gV_fep 
           (coords::float_type const E, coords::float_type const R, coords::float_type const r, coords::float_type const factor, coords::float_type &dV) const;
-        template< ::tinker::parameter::radius_types::T T_RADIUSTYPE > inline coords::float_type gV_fep_cut
-          (coords::float_type const E, coords::float_type const R, coords::float_type const r, coords::float_type const factor, coords::float_type const factor2, coords::float_type &dV, coords::float_type &alche2, coords::float_type &fV, coords::float_type &fV2) const;
-        // charge+vdw (no cutoff, no fep, no periodics)
-
+		/**vdw gradients FEP version with cutoff*/
+		template< ::tinker::parameter::radius_types::T T_RADIUSTYPE > inline coords::float_type gV_fep_cut
+		(coords::float_type const E, coords::float_type const R, coords::float_type const r, coords::float_type const factor, coords::float_type const factor2, coords::float_type &dV, coords::float_type &alche2, coords::float_type &fV, coords::float_type &fV2) const;
+		
+		/** charge+vdw energies (no cutoff, no fep, no periodics) */
         template< ::tinker::parameter::radius_types::T T_RADIUS_TYPE> 
         inline void e_QV  (coords::float_type const C, coords::float_type const E, coords::float_type const R, coords::float_type const r, 
           coords::float_type &e_c, coords::float_type &e_v) const;
-        // charge+vdw gradients
+        /** charge+vdw gradients (no cutoff, no fep, no periodics) */
         template< ::tinker::parameter::radius_types::T T_RADIUS_TYPE> 
         inline void g_QV  (coords::float_type const C, coords::float_type const E, coords::float_type const R, coords::float_type const r, 
           coords::float_type &e_c, coords::float_type &e_v, coords::float_type &dE) const;
 
+		/** charge+vdw gradients fep version (no cutoff, no periodics) */
         template< ::tinker::parameter::radius_types::T T_RADIUS_TYPE>
         inline void g_QV_fep  (coords::float_type const C, coords::float_type const E, coords::float_type const R, coords::float_type const r, 
           coords::float_type const c_out, coords::float_type const v_out,
           coords::float_type &e_c, coords::float_type &e_v, coords::float_type &dE) const;
 
-        // charge+vdw (cutoff, no fep, no periodics)
-
+        
+		//** charge+vdw energies (cutoff, no fep, no periodics) */
         template< ::tinker::parameter::radius_types::T T_RADIUS_TYPE> 
         inline void e_QV_cutoff  (coords::float_type const C, coords::float_type const E, coords::float_type const R, coords::float_type const r, 
           coords::float_type const fQ, coords::float_type const fV,coords::float_type &e_c, coords::float_type &e_v) const;
-        // charge+vdw gradients
+        /** charge+vdw gradients (cutoff, no fep, no periodics) */
         template< ::tinker::parameter::radius_types::T T_RADIUS_TYPE> 
         inline void g_QV_cutoff  (coords::float_type const C, coords::float_type const E, coords::float_type const R, coords::float_type const r, 
           coords::float_type const fQ, coords::float_type const fV, coords::float_type &e_c, coords::float_type &e_v, coords::float_type &dE) const;
-        template< ::tinker::parameter::radius_types::T T_RADIUS_TYPE> 
+		/** charge+vdw gradients fep version (cutoff, no periodics) */
+		template< ::tinker::parameter::radius_types::T T_RADIUS_TYPE> 
         inline void g_QV_fep_cutoff  (coords::float_type const C, coords::float_type const E, coords::float_type const R, coords::float_type const r, 
           coords::float_type const c_out, coords::float_type const v_out, coords::float_type const fQ, coords::float_type const fV, coords::float_type &e_c, coords::float_type &e_v, coords::float_type &dE) const;
 
-        // pairs functions
+        /** gradient function for non-bonded pairs */
         template< ::tinker::parameter::radius_types::T T_RADIUS_TYPE> 
         void g_nb_QV_pairs (coords::float_type &e_nb, coords::Representation_3D &grad_vector, 
           std::vector< ::tinker::refine::types::nbpair> const & pairs,
           scon::matrix< ::tinker::parameter::combi::vdwc, true> const & parameters);
 
+		/** gradient function for non-bonded pairs with cutoff */
         template< ::tinker::parameter::radius_types::T T_RADIUS_TYPE, bool PERIODIC> 
         void g_nb_QV_pairs_cutoff (coords::float_type &e_nb, coords::Representation_3D &grad_vector, 
           std::vector< ::tinker::refine::types::nbpair> const & pairs, 
           scon::matrix< ::tinker::parameter::combi::vdwc, true> const & parameters);
+
+		/** FEP gradient function for non-bonded pairs 
+		depends on the fact if the current atom is appearing or disappearing*/
+		template< ::tinker::parameter::radius_types::T T_RADIUS_TYPE, bool PERIODIC, bool IS_OUT>
+		void g_nb_QV_pairs_fep_io(coords::float_type &e_nb, coords::Representation_3D &grad_vector,
+			std::vector< ::tinker::refine::types::nbpair> const & pairs,
+			scon::matrix< ::tinker::parameter::combi::vdwc, true> const & parameters);
+
         
         //PME functions
         template < ::tinker::parameter::radius_types::T T_RADIUS_TYPE>
@@ -214,22 +234,6 @@ namespace energy
         void setchargestofepoutdgrid();
         void setchargestoallgrid();
         void sitecorrection(int &, int &, int &, int &, int &, int &, int &);
-
-        // FEP pair functions
-        template< ::tinker::parameter::radius_types::T T_RADIUS_TYPE> 
-        void g_nb_QV_pairs_fep_switch_periodic (coords::float_type &e_nb, coords::Representation_3D &grad_vector, 
-          std::vector< ::tinker::refine::types::nbpair> const & pairs, 
-          scon::matrix< ::tinker::parameter::combi::vdwc, true> const & parameters);
-
-        //template< ::tinker::parameter::radius_types::T T_RADIUS_TYPE, bool PERIODIC> 
-        //void g_nb_QV_pairs_fep (coords::float_type &e_nb, coords::Representation_3D &grad_vector, 
-        //  std::vector< ::tinker::refine::types::nbpair> const & pairs, 
-        //  scon::tsmatrix< ::tinker::parameter::combi::vdwc> const & parameters);
-
-        template< ::tinker::parameter::radius_types::T T_RADIUS_TYPE, bool PERIODIC, bool IS_OUT> 
-        void g_nb_QV_pairs_fep_io(coords::float_type &e_nb, coords::Representation_3D &grad_vector,
-          std::vector< ::tinker::refine::types::nbpair> const & pairs, 
-          scon::matrix< ::tinker::parameter::combi::vdwc, true> const & parameters);
 
       };
     }
