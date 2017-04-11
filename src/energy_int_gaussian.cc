c860b7011ad19f75013d86db909714fc8ba89fe6
Bastian <bastian.michels@stud-mail.uni-wuerzburg.de>
2017-04-11 13:15:43 +0200

    Change reading for ex-ex-transdipoles

--- src/energy_int_gaussian.cc
+++ src/energy_int_gaussian.cc
@@ -177,7 +177,7 @@ void::energy::interfaces::gaussian::sysCallInterfaceGauss::read_gaussianOutput(b
 
           if (buffer.find("Excited to excited state transition velocity") != std::string::npos) {el_dipm = false;}
 
-          std::sscanf(buffer.c_str(), "%i %i %lf %lf %lf", &state_i[i], &state_j[i], ex_ex_trans[i].x(), ex_ex_trans[i].y(), ex_ex_trans[i].z());
+          std::sscanf(buffer.c_str(), "%i %i %lf %lf %lf %*s %*s", &state_i[i], &state_j[i], ex_ex_trans[i].x(), ex_ex_trans[i].y(), ex_ex_trans[i].z());
 
         }
 
