71e6a1a33a265f85ffa8644c5ef1eb8d921a48bd
Bastian <bastian.michels@stud-mail.uni-wuerzburg.de>
2017-05-02 17:04:20 +0200

    Move debuggingoutput .close()

--- src/Couplings.cc
+++ src/Couplings.cc
@@ -58,10 +58,12 @@ test << V_el_tmp;
 
           V_el.push_back(V_el_tmp);
 
-          ZINDO(dim_coords);
-
 test << '7';
 test.close();
+
+          ZINDO(dim_coords);
+
+
 
           V_ex.push_back(0.5*(c_excitE[1] - c_excitE[0]));
 
