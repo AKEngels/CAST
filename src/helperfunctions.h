0bf04ee14dba0da2df97c60788dc359628928230
Dustin Kaiser <Dustin.Kaiser@gmx.net>
2017-08-09 18:19:47 +0200

    Add eucledean distance function to helperfunctions header

--- src/helperfunctions.h
+++ src/helperfunctions.h
@@ -1,5 +1,13 @@
 #pragma once
 #include "coords.h"
+#include <iostream>
+#include <string>
+#include <iterator>
+#include <algorithm>
+#include <vector>
+#include <functional>
+#include <cmath>
+#include <numeric>
 
 // Define Function to output molar mass of a coords object
 double sys_mass(coords::Coordinates &sys)
@@ -36,3 +44,17 @@ void short_ene_stream_h(
     strm << std::setw(w) << ("WW" + std::to_string(i));
   }
 }
+
+// VIA http://www.cplusplus.com/forum/general/209784/
+// Computes the distance between two std::vectors
+template <typename T>
+T	eucledeanDistance(const std::vector<T>& a, const std::vector<T>& b)
+{
+  std::vector<T>	auxiliary;
+
+  std::transform(a.begin(), a.end(), b.begin(), std::back_inserter(auxiliary),//
+    [](T element1, T element2) {return pow((element1 - element2), 2); });
+
+  return std::sqrt(std::accumulate(auxiliary.begin(), auxiliary.end(), 0.0));
+
+} // end template vectors_distance
