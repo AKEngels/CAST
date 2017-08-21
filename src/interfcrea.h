9c6d82fc8b1a08d6bddac86c4400ea30b398b82f
Bastian <bastian.michels@uni-wuerzburg.de>
2017-08-21 12:05:59 +0200

    Rermove old interface_creation function
    New function is in periodicCutout.h

--- src/interfcrea.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include<string>
-#include<vector>
-#include"coords.h"
-
-
-coords::Coordinates interface_creation(char, double, coords::Coordinates, coords::Coordinates);//creating second coordinates object for second structure
