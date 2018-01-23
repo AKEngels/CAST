45e9a8c2d524ff2acf1bc0280804f93cf8e9d316
Susanne Sauer <susanneTsauer@gmx.net>
2018-01-23 14:44:46 +0100

    remove devtest again

--- src/main.cc
+++ src/main.cc
@@ -293,13 +293,6 @@ int main(int argc, char **argv)
     {
     case config::tasks::DEVTEST:
     {
-      for (auto const& pes : *ci)
-      {
-        coords.set_xyz(pes.structure.cartesian);
-        coords.g();
-        coords.to_internal();
-        coords.to_xyz();
-      }
       // DEVTEST: Room for Development testing
       break;
     }
