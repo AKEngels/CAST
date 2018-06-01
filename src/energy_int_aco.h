165e59de0c5cf46f9d4f7ae484c2aeac08ba2d84
Susanne Sauer <susanneTsauer@gmx.net>
2018-06-01 14:10:43 +0200

    return gradients on external charges in ACO

--- src/energy_int_aco.h
+++ src/energy_int_aco.h
@@ -54,22 +54,22 @@ namespace energy
         /**update structure (account for topology or rep change, or just non-bonded list)*/
         void update(bool const skip_topology = false);
 
-		/**Energy function*/
-		coords::float_type e(void);
-		/**Energy+Gradient function*/
-		coords::float_type g(void);
-		/** Energy+Gradient+Hessian function
-		at the moment does nothing because Hessians are not implemented yet*/
-		coords::float_type h(void);
-		/** Optimization in the intface or interfaced program*/
-		coords::float_type o(void);
+		    /**Energy function*/
+		    coords::float_type e(void);
+		    /**Energy+Gradient function*/
+		    coords::float_type g(void);
+		    /** Energy+Gradient+Hessian function
+		    at the moment does nothing because Hessians are not implemented yet*/
+		    coords::float_type h(void);
+		    /** Optimization in the intface or interfaced program*/
+		    coords::float_type o(void);
 
         /**get charges*/
         std::vector<coords::float_type> charges() const override;
         /**overwritten function, should not be called*/
         std::vector<coords::Cartesian_Point> get_g_coul_mm() const override
         {
-          throw std::runtime_error("TODO: Implement electric field.\n");
+          return grad_ext_charges;
         }
         /**overwritten function, should not be called*/
         coords::Gradients_3D get_link_atom_grad() const override
