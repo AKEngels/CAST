ba87ae2d0e2658ef495ff63dd012ef366b631d97
susanne <susanneTsauer@gmx.net>
2018-10-01 14:15:13 +0200

    change update.sh to run tests

--- run_test.sh
+++
@@ -1,8 +0,0 @@
-git pull
-cd optional_files
-/apps/premake5/premake5 gmake
-cd project
-make config=testing_x64 CXX=g++-5
-cd ..
-cd build
-./CAST_linux_x64_testing
--- update.sh
+++ update.sh
@@ -5,3 +5,7 @@ cd project
 make config=release_x64 CXX=g++-5
 make config=python_release_x64 CXX=g++-5
 make config=armadillo_release_x64 CXX=g++-5
+make config=testing_x64 CXX=g++-5
+cd ..
+cd build
+./CAST_linux_x64_testing
