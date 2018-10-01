494a6cb0c78ceb420f8ddd44741e995160c99441
susanne <susanneTsauer@gmx.net>
2018-10-01 15:24:42 +0200

    add travis.yml

--- test.sh
+++ test.sh
@@ -1,10 +1,6 @@
-git pull
 cd optional_files
 /apps/premake5/premake5 gmake
 cd project
-make config=release_x64 CXX=g++-5
-make config=python_release_x64 CXX=g++-5
-make config=armadillo_release_x64 CXX=g++-5
 make config=testing_x64 CXX=g++-5
 cd ..
 cd build
