ae6b5e88360c3ef657fdb367295cccf0fb5351d9
Susanne Sauer <susanneTsauer@gmx.net>
2019-01-03 15:07:01 +0100

    continue with WHAM analysis in scripting and tutorial

--- optional_files/scripts/umbrella.py
+++ optional_files/scripts/umbrella.py
@@ -1,9 +1,12 @@
-### script to start an umbrella calculation on ECPC, things users probably want to change are marked with USER INPUT ###
+### script to start an umbrella calculation on ECPC ###
 
 import os
 import shutil
 
-steps = range(-180, 185, 5)  # to which values should the restraint be set? (USER INPUT)
+MOLECULE = "butan.arc"              # name of molecule file
+FORCEFIELD = "charmm22.prm"         # name of forcefield file
+STEPS = range(-180, 185, 5)         # to which values should the restraint be set?
+PLACEHOLDER = "UMBRELLA_RESTRAINT"  # placeholder in CAST.txt file that is replaced by step number
 
 """function that copies all necessary files for a calculation into folders and changes lines in inputfiles"""
 def run_calc(step):
@@ -12,8 +15,8 @@ def run_calc(step):
     os.mkdir("{}".format(step))  
 
     # copy necessary files to that folder (USER INPUT)
-    shutil.copy("butan.arc","{}/butan.arc".format(step))
-    shutil.copy("oplsaa_mod2.prm","{}/oplsaa_mod2.prm".format(step))
+    shutil.copy(MOLECULE,"{}/{}".format(step,MOLECULE))
+    shutil.copy(FORCEFIELD,"{}/{}".format(step,FORCEFIELD))
     shutil.copy("cast.sh", "{}/cast.sh".format(step))
     shutil.copy("CAST.txt", "{}/CAST.txt".format(step))
 
@@ -27,7 +30,7 @@ def run_calc(step):
     # important: set correct restraint in inputfile
     with open("{}/CAST.txt".format(step)) as inp:
         x = inp.read()
-        x = x.replace("UMBRELLA_RESTRAINT",str(float(step)))
+        x = x.replace(PLACEHOLDER,str(float(step)))
     with open("{}/CAST.txt".format(step),"w") as inp:
         inp.write(x)
 
@@ -36,5 +39,5 @@ def run_calc(step):
     os.popen("qsub cast.sh")
     os.chdir("..")
 
-for s in steps:
+for s in STEPS:
     run_calc(s)
