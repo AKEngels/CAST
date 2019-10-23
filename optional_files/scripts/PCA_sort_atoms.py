0a0da520705495e84db044edc13a8b6f9e33df5c
Susanne Sauer <susanneTsauer@gmx.net>
2019-10-23 11:33:10 +0200

    modify script to sort atoms of a PCA mode so that it returns a trajectory where the atoms of the mode appear (or disappear) according to their contribution

--- optional_files/scripts/PCA_sort_atoms.py
+++ optional_files/scripts/PCA_sort_atoms.py
@@ -1,20 +1,36 @@
 ### With this script you can process the file 'pca_stocksdelta.dat' from task PCAgen.
 ###
-### It reads the contribution of every atom for a given node (starting with 0)
-### and sorts the atoms by how much they contribute to this node.
+### It reads the contribution of every atom for a given mode (starting with 0)
+### and sorts the atoms by how much they contribute to this mode.
 ### The result is written into a file 'node<nodenumber>_atoms.csv'.
 ###
 ### Furthermore it prints the first X atoms in a format that you can copy into VMD
 ### where you can choose X as you desire.
+###
+### The last thing this script does is to write a tracefile of tinkerstructures
+### where the last structure contains only the atom with the biggest contribution
+### the structure before the two atoms with the biggest contribution and so on
+### until the first structure contains all atoms that contribute to the mode.
 
 MODE = 0
 NUMBER_OF_ATOMS = 100
+STRUCTURE = "ncov_full_19_21.arc"
+
+import os
 
 class Atom:
 
     def __init__(self, index, weight):
         self.index = index
         self.weight = weight
+
+class AtomWithCoords:
+
+    def __init__(self, element, x, y, z):
+        self.element = element
+        self.x = x
+        self.y = y
+        self.z = z
 
 def find_modeline(lines):
     for i,line in enumerate(lines):
@@ -47,13 +63,64 @@ def get_vmd_indices(number, atoms):
         string += "index " + str(atoms[i].index-1) + " or "
     print string[:-4]
 
+def read_atoms_from_structure(filename):
+    with open(filename) as inp:
+        lines = inp.readlines()
+
+    number_of_atoms = int(lines[0])
+
+    atoms = []
+    for i in range(number_of_atoms):
+        line = lines[i+1]
+        linelist = line.split()
+        element = linelist[1]
+        x = float(linelist[2])
+        y = float(linelist[3])
+        z = float(linelist[4])
+        atom = AtomWithCoords(element, x, y, z)
+        atoms.append(atom)
+        i=i+1
+    return atoms
+
+def create_partial_structure(indices, structure, total_size):
+    string = "{}\n".format(total_size)
+    for i,index in enumerate(indices):
+        atom = structure[index-1]
+        string += "{}  {}  {}  {}  {}\n".format(i+1, atom.element, atom.x, atom.y, atom.z)
+    number_of_dummys = total_size - len(indices)
+    for i in range(number_of_dummys):
+        string += "{}   H   0  0   0 \n".format(i+1+len(indices))
+    return string
+
+def create_strings_for_all_partial_structures(atoms, atoms_structure):
+    tinkerstructures = []
+    for i, a in enumerate(atoms):
+        indices_for_structure = []
+        for j in range(i+1):
+            indices_for_structure.append(atoms[j].index)
+        tinkerstructures.append(create_partial_structure(indices_for_structure, atoms_structure, len(atoms)))
+    return tinkerstructures
+
+def write_tinkerstructures_to_file(tinkerstructures):
+    for i,t in enumerate(tinkerstructures):
+        index = len(tinkerstructures) - i -1
+        with open("mode{}.arc".format(MODE), "a") as out:
+            out.write(tinkerstructures[index])
+
 with open("pca_stocksdelta.dat") as inp:
     lines = inp.readlines()
 
 i = find_modeline(lines)
 atoms = create_atomlist(i)
 atoms = sorted(atoms, key=lambda atom: atom.weight, reverse=True)
-write_to_file(atoms, "node{}_atoms.csv".format(MODE))
+write_to_file(atoms, "mode{}_atoms.csv".format(MODE))
 get_vmd_indices(NUMBER_OF_ATOMS, atoms)
+
+os.remove("mode{}.arc".format(MODE))
+atoms_structure = read_atoms_from_structure(STRUCTURE)
+tinkerstructures = create_strings_for_all_partial_structures(atoms, atoms_structure)
+write_tinkerstructures_to_file(tinkerstructures)
+
+
 
 
