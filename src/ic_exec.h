4821e4008b6bce1b0351d3c4c750082c6e46cb7e
Christian Schärf <christian.schaerf@stud-mail.uni-wuerzburg.de>
2019-11-05 16:17:03 +0100

    Implemented Z matrix generation.

--- src/ic_exec.h
+++ src/ic_exec.h
@@ -10,6 +10,7 @@
 #include "InternalCoordinates/Optimizer.h"
 #include "InternalCoordinates/InternalCoordinateUtilities.h"
 
+#include <boost/graph/depth_first_search.hpp>
 #include <boost/graph/breadth_first_search.hpp>
 
 #include <iostream>
@@ -18,7 +19,7 @@
 template <typename Inserter>
 class mst_visitor : public boost::default_bfs_visitor{
 public:
-  mst_visitor(Inserter inserter):
+  explicit mst_visitor(Inserter inserter):
     m_inserter{inserter}
   {}
 
@@ -30,6 +31,97 @@ public:
 
 private:
   Inserter m_inserter;
+};
+
+struct z_matrix_node : public ic_util::Node{
+  template <typename Graph>
+  void print_z_matrix_entry(std::ostream & out, Graph const& g, coords::Representation_3D const& c) const{
+    auto print_coord = [&out, &g, &c](auto coord_pair){
+
+      out << ' ' << g[coord_pair->second].m_position << ' ' << coord_pair->first.val(c);
+    };
+
+    out << element;
+    if (m_distance) {
+      print_coord(m_distance);
+      if (m_angle){
+        print_coord(m_angle);
+        if (m_dihedral){
+          print_coord(m_dihedral);
+        }
+      }
+    }
+    out << '\n';
+  }
+
+  boost::optional<std::pair<InternalCoordinates::BondDistance, std::size_t>> m_distance;
+  boost::optional<std::pair<InternalCoordinates::BondAngle, std::size_t>> m_angle;
+  boost::optional<std::pair<InternalCoordinates::DihedralAngle, std::size_t>> m_dihedral;
+
+  // Position in the Z matrix
+  std::size_t m_position;
+};
+
+template <typename Vertex, typename Graph>
+boost::optional<Vertex> get_parent_vertex(Vertex u, Graph g){
+  auto pair = boost::inv_adjacent_vertices(u, g);
+
+  // Look whether we have a parent vertex
+  if (pair.first == pair.second)
+    return boost::none;
+  else
+    return *pair.first;
+}
+
+template <typename Graph>
+class z_matrix_visitor : public boost::default_dfs_visitor {
+  using VertexDescriptor = typename boost::graph_traits<Graph>::vertex_descriptor;
+
+public:
+  explicit z_matrix_visitor(Graph &g, std::vector<VertexDescriptor> &z_matrix_order) :
+          m_graph{g},
+          m_z_matrix_order{z_matrix_order}
+          {}
+
+  void discover_vertex(VertexDescriptor u, Graph /*g*/) {
+    m_graph[u].m_position = m_curr_vertex;
+    m_z_matrix_order.emplace_back(u);
+
+    auto anchor = u;
+    std::vector<VertexDescriptor> curr_anchors;
+    for (std::size_t i = 0; i < 3; ++i) {
+      auto parent = get_parent_vertex(anchor, m_graph);
+      if (parent) {
+        curr_anchors.emplace_back(*parent);
+        // Construct the appropriate internal coordinates
+        switch (i) {
+          case 0:
+            m_graph[u].m_distance = std::make_pair(
+                    InternalCoordinates::BondDistance(m_graph[curr_anchors[0]], m_graph[u]), curr_anchors[0]);
+            break;
+          case 1:
+            m_graph[u].m_angle = std::make_pair(
+                    InternalCoordinates::BondAngle(m_graph[curr_anchors[1]], m_graph[curr_anchors[0]], m_graph[u]),
+                    curr_anchors[1]);
+            break;
+          case 2:
+            m_graph[u].m_dihedral = std::make_pair(
+                    InternalCoordinates::DihedralAngle(m_graph[curr_anchors[2]], m_graph[curr_anchors[1]],
+                                                       m_graph[curr_anchors[0]], m_graph[u]), curr_anchors[2]);
+            break;
+          default:
+            break;
+        }
+        anchor = *parent;
+      } else break;
+    }
+    ++m_curr_vertex;
+  }
+
+private:
+  std::size_t m_curr_vertex{1};
+  Graph &m_graph;
+  std::vector<VertexDescriptor> &m_z_matrix_order;
 };
 
 class ic_testing
@@ -108,23 +200,33 @@ public:
     std::vector<std::pair<std::size_t, std::size_t>> tree_edges;
     auto inserter = std::back_inserter(tree_edges);
     mst_visitor<decltype(inserter)> v(inserter);
-    boost::breadth_first_search(graph, 0, boost::visitor(v));
+    constexpr std::size_t root_index = 8;
+    boost::breadth_first_search(graph, root_index, boost::visitor(v));
 
-    boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, ic_util::Node> spanning_tree{
+    boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS, z_matrix_node> spanning_tree{
         tree_edges.begin(),
         tree_edges.end(),
         curGraphinfo.size()
     };
 
-    for (auto i = 0u; i < curGraphinfo.size(); ++i) {
+    for (std::size_t i = 0; i < curGraphinfo.size(); ++i) {
       spanning_tree[i].atom_serial = curGraphinfo.at(i).atom_serial;
       spanning_tree[i].atom_name = curGraphinfo.at(i).atom_name;
       spanning_tree[i].element = curGraphinfo.at(i).element;
       spanning_tree[i].cp = curGraphinfo.at(i).cp;
     }
 
+    std::vector<boost::graph_traits<decltype(spanning_tree)>::vertex_descriptor> z_matrix_order;
+    z_matrix_visitor<decltype(spanning_tree)> vis{spanning_tree, z_matrix_order};
+    boost::depth_first_search(spanning_tree, boost::visitor(vis).root_vertex(root_index));
+
     std::ofstream s("spanning-tree.txt");
     boost::write_graphviz(s, spanning_tree, boost::make_label_writer(boost::get(&ic_util::Node::atom_name, spanning_tree)));
+
+    std::ofstream o("z-matrix.txt");
+    for (auto curr_vertex : z_matrix_order)
+      spanning_tree[curr_vertex].print_z_matrix_entry(o, spanning_tree, cp_vec2);
+    o.close();
 
     // output graphviz file from graph
     /*graph.visualize_graph("Graphviz");
