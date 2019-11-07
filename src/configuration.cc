7051c32f27bdcbc55d160f36c7bb15e98530cf98
Susanne Sauer <susanneTsauer@gmx.net>
2019-11-07 13:13:53 +0100

    let user set penalty for 2D spline by hand

--- src/configuration.cc
+++ src/configuration.cc
@@ -1569,6 +1569,8 @@ void config::parse_option(std::string const option, std::string const value_stri
   }
   else if (option.substr(3) == "2d_gridpoints")
     Config::set().coords.umbrella.pmf_ic.gridpoints = ints_from_string(value_string);
+  else if (option.substr(3) == "2d_penalty")
+    Config::set().coords.umbrella.pmf_ic.penalty = std::stod(value_string);
     
   else if (option.substr(3) == "PMF_IC")
     Config::set().coords.umbrella.pmf_ic.use = bool_from_iss(cv);
